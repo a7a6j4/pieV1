5d132dc70186597c849f092beaf2fa1419a0d96a
AniefonPie <jasuquo@gmail.com>
2026-02-28 11:59:18 +0100

    income advisory improvements

--- router/v1/advisory.py
+++ router/v1/advisory.py
@@ -572,26 +572,26 @@ async def getNewPortfolioAllocation(
     tenor = schemas.tenor_map[frequency]
     if frequency == schemas.Frequency.MONTHLY:
       base_query = base_query.where(or_(model.Deposit.maxTenor.in_([30, 31]), model.Variable.horizon == 1, model.Variable.attributes.has(model.VariableAttributes.distribution == schemas.Frequency.MONTHLY)))
-      required_amount = portfolio.income.amount * 12 / ((product.rate if product.category == schemas.ProductClass.DEPOSIT else 80 if product.currency == schemas.Currency.USD else 200) / 100)
     elif frequency == schemas.Frequency.QUARTERLY:
       base_query = base_query.where(or_(model.Deposit.maxTenor.in_([90, 91, 92]), model.Variable.horizon == 1, model.Variable.attributes.has(model.VariableAttributes.distribution == schemas.Frequency.QUARTERLY)))
-      required_amount = portfolio.income.amount * 4 / ((product.rate if product.category == schemas.ProductClass.DEPOSIT else 80 if product.currency == schemas.Currency.USD else 200) / 100)
 
     elif frequency == schemas.Frequency.SEMIANNUALLY:
       base_query = base_query.where(or_(model.Deposit.maxTenor.in_([180, 181, 182]), model.Variable.horizon == 1, model.Variable.attributes.has(model.VariableAttributes.distribution == schemas.Frequency.SEMIANNUALLY)))
-      required_amount = portfolio.income.amount * 2 / ((product.rate if product.category == schemas.ProductClass.DEPOSIT else 80 if product.currency == schemas.Currency.USD else 200) / 100)
     elif frequency == schemas.Frequency.ANNUALLY:
       base_query = base_query.where(or_(model.Deposit.maxTenor.in_([364, 365, 366]), model.Variable.horizon == 1, model.Variable.attributes.has(model.VariableAttributes.distribution == schemas.Frequency.ANNUALLY)))
-      required_amount = portfolio.income.amount / ((product.rate if product.category == schemas.ProductClass.DEPOSIT else 80 if product.currency == schemas.Currency.USD else 200) / 100)
+
     products = db.execute(base_query.limit(3)).scalars().all()
 
     for product in products:
+
       recomendation = {
         "product": product,
-        "estAnnualReturn": product.rate if product.category == schemas.ProductClass.DEPOSIT else 8 if product.currency == schemas.Currency.USD else 20
+        "estAnnualReturn": product.rate if product.category == schemas.ProductClass.DEPOSIT else 8 if product.currency == schemas.Currency.USD else 20,
       }
       if portfolio.income.amount is not None:
-        target_income = portfolio.income.amount
+        numerator = portfolio.income.amount * 12 if frequency == schemas.Frequency.MONTHLY else portfolio.income.amount * 4 if frequency == schemas.Frequency.QUARTERLY else portfolio.income.amount * 2 if frequency == schemas.Frequency.SEMIANNUALLY else portfolio.income.amount
+        denominator = ((product.rate if product.category == schemas.ProductClass.DEPOSIT else 80 if product.currency == schemas.Currency.USD else 200) / 100) / 1 if portfolio.income.frequency == schemas.Frequency.ANNUALLY else 12 if portfolio.income.frequency == schemas.Frequency.MONTHLY else 4 if portfolio.income.frequency == schemas.Frequency.QUARTERLY else 2 if portfolio.income.frequency == schemas.Frequency.SEMIANNUALLY else 1
+        required_amount = numerator / denominator
         recomendation["amount"] = required_amount
 
       result.append(recomendation)
